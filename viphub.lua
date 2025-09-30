f1c0f83c281f3f1e2d46fdf80e30f6a85e09db61
raffipedia <159051073+raffipedia@users.noreply.github.com>
2025-09-30 15:53:02 +0700

    Create viphub.lua

--- viphub.lua
+++ viphub.lua
@@ -123,12 +123,14 @@ ExtraTab:CreateSlider({
     end,
 })
 
-local flyLoader = loadstring(game:HttpGet("https://raw.githubusercontent.com/raffipedia/base-hub/main/Fly.lua"))
-if flyLoader then
-    flyLoader()(Window)
-end
-
+-- Load Mt-Atin
 local atinLoader = loadstring(game:HttpGet("https://raw.githubusercontent.com/raffipedia/base-hub/main/Mt-Atin.lua"))
 if atinLoader then
     atinLoader()(Window)
+end
+
+-- Load Fly
+local flyLoader = loadstring(game:HttpGet("https://raw.githubusercontent.com/raffipedia/base-hub/main/Fly.lua"))
+if flyLoader then
+    flyLoader()(Window)
 end
